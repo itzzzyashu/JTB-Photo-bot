50a5caddcfe41afecf9856ecd04b926ac33f0fc8
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-08-25 19:53:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,23 @@
+//File No. 1
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         ApiContextInitializer.init();
 
         TelegramBotsApi botsApi = new TelegramBotsApi();
 
-        try {
+        try
+        {
             botsApi.registerBot(new PhotoBot());
-        } catch (TelegramApiException e) {
+        }
+        catch (TelegramApiException e)
+        {
             e.printStackTrace();
         }
         System.out.println("PhotoBot successfully started!");
