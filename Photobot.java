a816e39cf9d122e7f52e5c0b947c5a840e630871
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-08-25 19:56:18 +0530

    Update Photobot.java

--- Photobot.java
+++ Photobot.java
@@ -36,3 +36,25 @@ public class PhotoBot extends TelegramLongPollingBot {
         return "12345:qwertyuiopASDGFHKMK";
     }
 }
+
+
+@Override
+public void onUpdateReceived(Update update) {
+
+    // We check if the update has a message and the message has text
+    if (update.hasMessage() && update.getMessage().hasText()) {
+        // Set variables
+        String message_text = update.getMessage().getText();
+        long chat_id = update.getMessage().getChatId();
+        SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+        try {
+            sendMessage(message); // Sending our message object to user
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+        // Message contains photo
+    }
+}
