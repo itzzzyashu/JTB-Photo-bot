7735012452a455dea81d0522dcc9d9eabbeec0ea
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-08-25 19:55:00 +0530

    Create Photobot.java

---
+++ Photobot.java
@@ -0,0 +1,38 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class PhotoBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // Return bot username
+        // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+        return "PhotoBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // Return bot token from BotFather
+        return "12345:qwertyuiopASDGFHKMK";
+    }
+}
