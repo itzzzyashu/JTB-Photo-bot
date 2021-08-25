faa4d841a0af4efda8ad6c8f34d7af828f775b1c
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-08-25 19:23:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -19,3 +19,100 @@ public class Main
         System.out.println("PhotoBot successfully started!");
     }
 }
+// File No. 2
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class PhotoBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // Return bot username
+        // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+        return "PhotoBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // Return bot token from BotFather
+        return "12345:qwertyuiopASDGFHKMK";
+    }
+}
+
+// File No. 3
+@Override
+public void onUpdateReceived(Update update) {
+
+    // We check if the update has a message and the message has text
+    if (update.hasMessage() && update.getMessage().hasText()) {
+        // Set variables
+        String message_text = update.getMessage().getText();
+        long chat_id = update.getMessage().getChatId();
+        SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+        try {
+            sendMessage(message); // Sending our message object to user
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    } else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+        // Message contains photo
+    }
+}
+// File No. 4
+else if (update.hasMessage() && update.getMessage().hasPhoto()) {
+    // Message contains photo
+    // Set variables
+    long chat_id = update.getMessage().getChatId();
+
+    // Array with photo objects with different sizes
+    // We will get the biggest photo from that array
+    List<PhotoSize> photos = update.getMessage().getPhoto();
+    // Know file_id
+    String f_id = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getFileId();
+    // Know photo width
+    int f_width = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getWidth();
+    // Know photo height
+    int f_height = photos.stream()
+                    .sorted(Comparator.comparing(PhotoSize::getFileSize).reversed())
+                    .findFirst()
+                    .orElse(null).getHeight();
+    // Set photo caption
+    String caption = "file_id: " + f_id + "\nwidth: " + Integer.toString(f_width) + "\nheight: " + Integer.toString(f_height);
+    SendPhoto msg = new SendPhoto()
+                    .setChatId(chat_id)
+                    .setPhoto(f_id)
+                    .setCaption(caption);
+    try {
+        sendPhoto(msg); // Call method to send the photo with caption
+    } catch (TelegramApiException e) {
+        e.printStackTrace();
+    }
+}
